5af77d30f65db2046f87ecfb444f3f002e2bc5fd
vitikevich <vitikevich@gmail.com>
2020-03-16 16:34:29 +0700

    # Актуальные данные

--- bids_count/context_processors.py
+++ bids_count/context_processors.py
@@ -2,4 +2,4 @@ from bids_count.models import DjangoAdminLog
 
 
 def bids_count(request):
-    return {'bids_count': DjangoAdminLog.objects.count()}
+    return {'bids_count': DjangoAdminLog.created_in_current_month().count()}
