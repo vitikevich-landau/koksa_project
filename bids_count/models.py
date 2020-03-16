79f4fb5f447a3b92503ed8db04dffdc63d2ebab9
vitikevich <vitikevich@gmail.com>
2020-03-16 16:52:06 +0700

    # Inspected db # Add method created_in_current_month

--- bids_count/models.py
+++ bids_count/models.py
@@ -5,11 +5,21 @@ from django.db import models
 
 # Create your models here.
 class DjangoAdminLog(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    action_time = models.DateTimeField()
+    # object_id = models.TextField(blank=True, null=True)
+    # object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    # change_message = models.TextField()
+    # content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    # user = models.ForeignKey(AuthUser, models.DO_NOTHING)
 
-    def published_in_the_current_month(self):
-        return self.objects.filter(created__month=datetime.now().month)
+    @staticmethod
+    def created_in_current_month():
+        return DjangoAdminLog.objects\
+            .filter(action_time__month=datetime.now().month)\
+            .filter(action_flag=1)
 
     class Meta:
         managed = False
         db_table = 'django_admin_log'
+
